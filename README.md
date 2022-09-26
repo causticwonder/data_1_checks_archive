eb64a1e8d6df1a5740ca75e308df2fa8380b96b5
causticwonder <112646366+causticwonder@users.noreply.github.com>
2022-09-26 14:39:38 -0400

    create data checks python file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # data_1_checks
+
+Data Analysis 1 Assignment Checks Repo
+
+First created: 9/26/22
+
+#1 print "Hello World!" prompt
+#2 create list populate with several values and print one value
+#3 create dictionary populated with 2 keys and 2 values and print one value
+#4 create tuple with 4 values and print one value
---
+++ check1.py
@@ -0,0 +1,14 @@
+#print "Hello World!" prompt
+print('Hello world!')
+
+#create list populate with several values and print one value
+Pets = ['Frank', 'Lilly', 'Hank', 'Tina', 'CiCi']
+print(Pets[2])
+
+#create dictionary populated with 2 keys and 2 values and print one value
+my_pet_dictionary = {'dog': 'Frank', 'cat': 'Lilly'}
+print(my_pet_dictionary['cat'])
+
+#create tuple with 4 values and print one value
+ages = (41, 38, 12, 11)
+print(3)
